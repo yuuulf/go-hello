28671925c4fefe4b2fb09e3aebb543aaeff048eb
Yui Tatano <smharb525@gmail.com>
2019-12-01 18:29:21 +0900

    dataSourceとportを環境変数から取る

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/yuuulf/go-hello/pkg/db"
 	"github.com/yuuulf/go-hello/pkg/server"
 
@@ -8,12 +10,20 @@ import (
 )
 
 func main() {
-	err := db.Init("root:root@tcp(127.0.0.1:3307)/sample")
+	dataSourceName := os.Getenv("DATA_SOURCE_NAME")
+	if dataSourceName == "" {
+		dataSourceName = "root:root@tcp(127.0.0.1:3306)/sample"
+	}
+	err := db.Init(dataSourceName)
 	if err != nil {
 		panic(err.Error())
 	}
 	defer db.Close()
 
 	s := server.New()
-	s.Start()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":1323"
+	}
+	s.Start(port)
 }
